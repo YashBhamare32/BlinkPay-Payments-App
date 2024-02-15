778be5e3026dceb47e85869ac32e06de38de4f94
YashBhamare32 <bobbybhamare32@gmail.com>
2024-02-15 20:50:31 +0530

    deploy test 11

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,13 @@ const app = express();
 //     origin: "https://blinkpay-frontend.vercel.app/send" // frontend URI (ReactJS)
 // }
 
-
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(cors()); 
 // app.use(cors(corsOptions));
 app.use(express.json()); 
