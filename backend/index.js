a3a9fee860ec5cdf24af5a9d9a7bd853af8e4573
YashBhamare32 <bobbybhamare32@gmail.com>
2024-02-15 21:03:57 +0530

    deploy test 12

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    res.header("HTTP/1.1 200 OK")
       next();
     });
 app.use(cors()); 
