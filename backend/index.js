4f9b1c657959927cd566df03a961110e63a4a2af
YashBhamare32 <bobbybhamare32@gmail.com>
2024-02-16 11:19:00 +0530

    deploy test 13

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,27 @@ const app = express();
 //     origin: "https://blinkpay-frontend.vercel.app/send" // frontend URI (ReactJS)
 // }
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    res.header("HTTP/1.1 200 OK")
-      next();
-    });
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
 app.use(cors()); 
 // app.use(cors(corsOptions));
 app.use(express.json()); 
@@ -28,4 +41,5 @@ app.use(express.json());
 app.use("/api/v1" , mainRouter); 
 
 app.listen(3000);
+module.exports = allowCors(handler);
 module.exports = app;
