80fea22faa27528fcc396cc104e86e54af5f710a
YashBhamare32 <bobbybhamare32@gmail.com>
2024-02-21 21:28:49 +0530

    revert

--- backend/index.js
+++ backend/index.js
@@ -12,36 +12,34 @@ app.use(cors());
 //     origin: "https://blinkpay-frontend.vercel.app/send" // frontend URI (ReactJS)
 // }
 
-// const allowCors = fn => async (req, res) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   // another common pattern
-//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   )
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).end()
-//     return
-//   }
-//   return await fn(req, res)
-// }
-// const handler = (req, res) => {
-//   const d = new Date()
-//   res.end(d.toString())
-// }
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
 
 // app.use(cors()); 
 // app.use(cors(corsOptions));
 app.use(express.json()); 
 
-app.get('/', (req, res) => {
-  res.send("J");
-})
+
 app.use("/api/v1" , mainRouter); 
 
 app.listen(3000);
-// module.exports = allowCors(handler);
+module.exports = allowCors(handler);
 module.exports = app;
